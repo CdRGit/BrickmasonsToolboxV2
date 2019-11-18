475d57061cb5d8406696aa066e6f95ed7d066e2f
Casper de Regt <casper@shadowmaker.nl>
2019-11-18 20:32:53 +0100

    Started Working

---
+++ BrickmasonsToolboxV2/Integrations/MainFormOutput.cs
@@ -0,0 +1,34 @@
+﻿using CustomProgrammingLanguage.OutputMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickmasonsToolboxV2.Integrations
+{
+    public class MainFormOutput : IOutput
+    {
+        private MainForm form;
+
+        public MainFormOutput(MainForm form)
+        {
+            this.form = form;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteLine(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
